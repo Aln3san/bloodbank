817e2f54e2adafb79a783527d2620a803aa39dac
Aln3san <anas.aln3san.com>
2025-09-24 17:31:17 +0300

    contact us seeder

---
+++ database/seeders/Api/ContactSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders\Api;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ContactSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    DB::table('contacts')->insert([
+      [
+        'subject' => 'you must improve your app',
+        'massege' => 'your app is very good but you must improve it more and more',
+        'client_id' => 1,
+        'created_at' => now(),
+        'updated_at' => now()
+      ]
+    ]);
+  }
+}
